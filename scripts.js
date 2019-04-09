0294ceb8f5aa0f72c3edc5d501f7aeabd7360f1f
dpeet <devondpeet@gmail.com>
2019-04-09 08:09:38 -0400

    add this

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
-function submitForm() {
+function submitForm(this) {
     // Get the first form with the name
     // Usually the form name is not repeated
     // but duplicate names are possible in HTML
     // Therefore to work around the issue, enforce the correct index
+    print(this)
     let frm = document.getElementById('MCForm');
     frm.submit(); // Submit the form
     frm.reset();  // Reset all form data
     console.log("form submitted")
     document.getElementsByClassName("confirmation")[0].style.display="inherit"
-    return true; // Prevent page refresh
+    return true; 
  }
