eb24e3f99379e16876f68765a899ccd229af90c3
dpeet <devondpeet@gmail.com>
2019-04-09 08:32:47 -0400

    get it to work for now

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ function submitForm(formContext) {
     // but duplicate names are possible in HTML
     // Therefore to work around the issue, enforce the correct index
     console.log(formContext)
-    let frm = document.getElementById('MCForm');
-    frm.submit(); // Submit the form
-    frm.reset();  // Reset all form data
+    // let frm = document.getElementById('MCForm');
+    formContext.submit(); // Submit the form
+    // formContext.reset();  // Reset all form data
     console.log("form submitted")
     document.getElementsByClassName("confirmation")[0].style.display="inherit"
     return true; 
