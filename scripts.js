a1e09ac117a33f9910f5f7c774f6c692f4503772
dpeet <devondpeet@gmail.com>
2019-04-09 08:17:44 -0400

    revert

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // function submitForm(this) {
 //     // Get the first form with the name
 //     // Usually the form name is not repeated
@@ -35,3 +36,17 @@
   }
 
   attachFormSubmitEvent("MCForm")
+=======
+function submitForm() {
+    // Get the first form with the name
+    // Usually the form name is not repeated
+    // but duplicate names are possible in HTML
+    // Therefore to work around the issue, enforce the correct index
+    let frm = document.getElementById('MCForm');
+    frm.submit(); // Submit the form
+    frm.reset();  // Reset all form data
+    console.log("form submitted")
+    document.getElementsByClassName("confirmation")[0].style.display="inherit"
+    return true; // Prevent page refresh
+ }
+>>>>>>> parent of 0294ceb... add this
