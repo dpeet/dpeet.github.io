71b7ad73ac44329649af3e2b358b1b155082a4bf
dpeet <devondpeet@gmail.com>
2019-04-09 08:55:28 -0400

    add color change

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@ function submitForm(formContext) {
     // but duplicate names are possible in HTML
     // Therefore to work around the issue, enforce the correct index
     console.log("submit")
+    document.getElementsByClassName("confirmation")[0].style.display="inherit"
     return true; 
     console.log(formContext)
     // let frm = document.getElementById('MCForm');
     formContext.submit(); // Submit the form
     // formContext.reset();  // Reset all form data
     console.log("form submitted")
-    document.getElementsByClassName("confirmation")[0].style.display="inherit"
     return true; 
  }
