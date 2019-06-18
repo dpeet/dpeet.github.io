b58e6e0d4c511a32a72ad6a399fa1add2ae4499e
dpeet <devondpeet@gmail.com>
2019-06-17 20:13:15 -0400

    add timeout/reload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 function submitForm(formContext) {
     console.log("submit")
     document.getElementsByClassName("confirmation")[0].style.display="inherit"
+    setTimeout(function(){window.location.reload();},3000)
     return true; 
  }
 
